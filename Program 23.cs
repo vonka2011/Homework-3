2ff7214204d12ee5ba408fab13c1e11a295d4837
vonka2011 <108064137+vonka2011@users.noreply.github.com>
2022-09-13 23:33:27 +0300

    Add files via upload

---
+++ Program 23.cs
@@ -0,0 +1,26 @@
+﻿// Напишите прогр.,кот. принимает на вход число N и выдает табл.кубов от 1 до N/
+void CubsNumbers(int number)
+{
+    if (number < 0)
+    {
+        Console.WriteLine("Вы ввели отрицательное число, поэтому мы сами его преобразовали в положительное ;)");
+        number = number * -1;
+    }
+    if (number == 0)
+    {
+        Console.WriteLine("Вы ввели 0, чисел нет!");
+    }
+    int count = 1;
+    while (count <= number)
+    {
+        int result = count * count * count;
+        Console.Write(result + " ");
+        count++;
+    }
+}
+
+Console.Write("Введите число N и программа выдаст таблицу кубов чисел от 1 до N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubsNumbers(number);
+Console.WriteLine(); 
